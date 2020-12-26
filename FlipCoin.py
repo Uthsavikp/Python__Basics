76c6a07215af2f2cfba829919dca7d944a46e01f
uthsa <uthsavi29@gmail.com>
2020-12-26 17:56:38 +0530

    Added:TTo Take User Input For Number Of Times To Flip A Coin And Print The Percentage Of Head Vs Tail

---
+++ FlipCoin.py
@@ -0,0 +1,23 @@
+'''
+* @Author: Uthsavi KP
+* @Date: 2020-12-26 17:50:32  
+* @Last Modified by:   Uthsavi KP
+* @Last Modified time: 2020-12-26 17:50:32
+'''
+
+import random
+
+def flipCoin():
+    noOfFlip = int(input("Please Enter The Number Of Times You Want To Flip A Coin: "))
+    tail = 0
+    head = 0
+    for toss in range(noOfFlip):
+        toss=random.random() #will generate a random floating-point number in the range (0.0, 1.0)
+        if toss <0.5:
+            tail+=1
+        else:
+            head+=1
+        percentHead=(head/noOfFlip)*100
+        percentTail=100 - percentHead 
+    print("Percentage Of Head:",percentHead,"vs","Percentage Of Tail:",percentTail)
+flipCoin()
