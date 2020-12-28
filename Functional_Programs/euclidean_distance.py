eae5ce029c11fe54da88e5ffa43341bedc1e9bc8
uthsa <uthsavi29@gmail.com>
2020-12-28 18:38:42 +0530

    Refactored:The Distance Formula By Using math.pow

--- Functional_Programs/euclidean_distance.py
+++ Functional_Programs/euclidean_distance.py
@@ -1,8 +1,8 @@
 '''
 * @Author: Uthsavi KP
-* @Date: 2020-12-28 17:05:27
+* @Date: 2020-12-28 18:26:15
 * @Last Modified by:   Uthsavi KP
-* @Last Modified time: 2020-12-28 17:05:27
+* @Last Modified time: 2020-12-28 18:26:15
 * @Title:To calculate euclidean distance of x and y co-ordinates.
 '''
 
@@ -15,8 +15,8 @@ class Distance:
             x_point = int(input("Enter The Number For X-Point : ")) 
             y_point = int(input("Enter The Number For Y-Point : "))
             
-            distance=math.sqrt((x_point*x_point) + (y_point*y_point)) #Formula To Calculate Euclidean Distance
-            print("Euclidean Distance From The Points (x,y) Is :",distance)
+            distance_formula=math.sqrt(math.pow(x_point,2) + math.pow(y_point,2))#Formula To Calculate Euclidean Distance
+            print("Euclidean Distance From The Points (x,y) Is :",distance_formula)
         except Exception as err:
             print("** Error **", err) 
             print("Enter Only Numbers")
